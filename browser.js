60246e4f135c0ec9d850a47aef82c44602ca4d26
nick thomas <nichoth@gmail.com>
2022-07-10 14:05:31 -0700

    use different browser file

--- browser.js
+++ browser.js
@@ -1,8 +1,13 @@
+const base64url = require('base64url');
+
 function getHash (data) {
     return webcrypto.subtle.digest('SHA-256', data).then(buf => {
-        return (Array.from(new Uint8Array(buf), b => {
-            return b.toString(16).padStart(2, '0')
-        }).join('')) + '.sha256'
+        return base64url.encode(buf) + '.sha256'
+
+        // this is hex version
+        // return (Array.from(new Uint8Array(buf), b => {
+        //     return b.toString(16).padStart(2, '0')
+        // }).join('')) + '.sha256'
     })
 }
 
--- test/index.js
+++ test/index.js
@@ -9,10 +9,10 @@ test('creates a hash', t => {
         t.ok(hashName, 'should create a hash filename')
         t.ok(hashName.includes('.sha256'), 'should use "sha256" by default')
         // base64url version
-        // t.equal(hashName, 'Ho1XhW2dp4bNJLZrYkurZPxlUhqrknD_Uu_nDp-KnMg.sha256',
-        //     'should return the expected hash, url version')
-        t.equal(hashName, '1e8d57856d9da786cd24b66b624bab64fc65521aab9270ff52efe70e9f8a9cc8.sha256',
-            'should return the expected hash, hex version')
+        t.equal(hashName, 'Ho1XhW2dp4bNJLZrYkurZPxlUhqrknD_Uu_nDp-KnMg.sha256',
+            'should return the expected hash, url version')
+        // t.equal(hashName, '1e8d57856d9da786cd24b66b624bab64fc65521aab9270ff52efe70e9f8a9cc8.sha256',
+        //     'should return the expected hash, hex version')
         t.end()
     })
 })
