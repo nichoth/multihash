2ae63c1122edb42fb04689b7cabfbd80dc6f11b4
nick thomas <nichoth@gmail.com>
2022-08-13 21:56:25 -0700

    comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { webcrypto } = require('one-webcrypto')
-const base64url = require('base64url');
+const base64url = require('base64url')
 
+// data should be Uint8Array
+const common = await TestCommon.create()
 function getHash (data) {
     return webcrypto.subtle.digest('SHA-256', data).then(buf => {
         return base64url.encode(buf) + '.sha256'
